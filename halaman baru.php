5c80aba8bb4d96aea14ec105351581619a983b66
NurmedinaMaulidiah <93064939+NurmedinaMaulidiah@users.noreply.github.com>
2022-10-09 15:25:07 +0800

    Add files via upload

---
+++ halaman baru.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link  href="'https://fonts.googleapis.com/css2?family=Bungee+Spice&family=Dancing+Script&family=Dangrek&family=Roboto:ital,wght@1,300&display=swap'">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Cafe</title>
+    <link rel="stylesheet" href="halaman baru2.css">
+</head>
+<body bgcolor="#fecb3a">
+    <nav>
+        <div class="wrapper">
+            <div class="menu">
+                <ul>
+                    <li class="button1"><a href="admin.php"><b>Home</b></a></h1></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+    <div class = "paragraf">
+    <p align = "center"><br><br><b>About Me</b></p>
+     <p align="center">Namaku Dinaaaa,<br>aku lahir di Marangakayu tanggal 6 Mei 2003 <br> aku kuliah di salah satu universitas yang ada di Kalimantan Timur <br> tepatnya Univesitas Mulawarman prodi Informatika angkatan 2021. <br>KALO GA MAU STRESS JGN MASUK SINI YGY!!!</p> <br>
+        <i><a href="https://instagram.com/dinamauldh?igshid=ZjA0NjI3M2I="><img src = 'logo-ig.png' width="25px" height="25px"></a>:@dinamauldh <br> <a href="mailto:nurmedinamaulidiah@gmail.com="><img src = 'email.png' width="25px" height="25px"></a>:nurmedinamaulidiah@gmail.com <br> <a href="https://open.spotify.com/user/ql9okj5lj2yaivzu3d4q185oy?si=b0c8bfbafb0243d1"><img src = 'spotify.png' width="25px" height="25px"></a>:dinamauldh</a></i><br>
+    <br><hr> <br>
+     <p align = "center"><b>About This Web</b></p>
+     <p align="center">Website ini merupakan salah satu website yang bisa kalian gunakan,<br>untuk melihat menu, daftar harga dan juga memesan menu pada fantastic food<br><br>
+     
+     
+     
+    </div>
+    <script src="uts.js"></script>
+    <script>src="jquery"</script>
+    <script> var paragraf = document.getElementsByClassName("paragraf");
+        console.log(paragraf);
+    </script> 
+    <footer>
+        <b>Copyright &copy; 2022</b>
+   <b>Designed by dinamauldh</b>
+    </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+   if(isset($_POST['login_admin'])) {
+        session_start();
+        header("Location: login-admin.php");
+   }
+   if(isset($_POST['login_user'])) {
+    session_start();
+    header("Location: login-user.php");
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <title>Login</title>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+   <link href="https://fonts.googleapis.com/css?family=Nunito:600,700,900" rel="stylesheet">
+   <link href="style4.css" rel="stylesheet">
+
+</head>
+
+<body id="body">
+   <div id="login-card" class="card">
+      <div class="card-body">
+         <h2 class="text-center">Pilihan Login</h2>
+         <br>
+         <form action="" method="POST">
+            <button type="submit" name="login_admin" id="email" class="btn btn-primary deep-purple btn-block ">SIGN IN AS ADMIN</button>
+            <button type="submit" name="login_user" id="email" class="btn btn-primary deep-purple btn-block ">SIGN IN AS USER</button>
+            <br>
+            <br>
+            <div id="btn" class="text-center">
+               <button type="button" class="btn btn-primary btn-circle btn-sm"><i class="fa fa-facebook"></i></button>
+               <button type="button" class="btn btn-danger btn-circle btn-sm"><i class="fa fa-google"></i></button>
+               <button type="button" class="btn btn-info btn-circle btn-sm"><i class="fa fa-instagram"></i></button>
+            </div>
+         </form>
+
+
+      </div>
+    </div>
+</body>
+</html>
